3fd43ccdd34444db71b04d129ad2e4cf04b2e1c6
Joseph <joe.hancock95@gmail.com>
2020-10-28 17:59:07 -0500

    added data

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-TESTER = document.getElementById('tester');
-Plotly.newPlot(TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16]
+annualHomicide = document.getElementById('annualHomicide');
+Plotly.newPlot(annualHomicide, [{
+    x: [2013, 2014, 2015, 2016, 2017, 2018, 2019],
+    y: [27, 37, 33, 42, 20, 32, 12]
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide13 = document.getElementById('monthlyHomicide13');
+Plotly.newPlot(monthlyHomicide13, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: [0, 3, 2, 4, 0, 2, 5, 0, 4, 2, 2, 3]
 }], {
     margin: { t: 0 }
 });
