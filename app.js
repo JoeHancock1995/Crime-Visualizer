a4ba4e7f66f7683abb069e222e32cb9aa7e455b8
Joseph <joe.hancock95@gmail.com>
2020-10-31 03:54:21 -0500

    added code for several more graphs

--- app.js
+++ app.js
@@ -13,3 +13,62 @@ Plotly.newPlot(monthlyHomicide13, [{
 }], {
     margin: { t: 0 }
 });
+
+monthlyHomicide14 = document.getElementById('monthlyHomicide14');
+Plotly.newPlot(monthlyHomicide14, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide15 = document.getElementById('monthlyHomicide15');
+Plotly.newPlot(monthlyHomicide15, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide16 = document.getElementById('monthlyHomicide16');
+Plotly.newPlot(monthlyHomicide16, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide17 = document.getElementById('monthlyHomicide17');
+Plotly.newPlot(monthlyHomicide17, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide18 = document.getElementById('monthlyHomicide18');
+Plotly.newPlot(monthlyHomicide18, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+monthlyHomicide19 = document.getElementById('monthlyHomicide19');
+Plotly.newPlot(monthlyHomicide19, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
+
+
+//Since this is the present year and will require adding an API may be more practical if one can be found rather than adding onto monthly. 
+// Create a CSV 
+monthlyHomicide20 = document.getElementById('monthlyHomicide20');
+Plotly.newPlot(monthlyHomicide20, [{
+    x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    y: []
+}], {
+    margin: { t: 0 }
+});
